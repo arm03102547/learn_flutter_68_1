35bede162848b55b9d7bc2b32bcbe6558cbff379
arm03102547 <arm03102547@gmail.com>
2026-08-08 14:03:15 +0700

    Step 6 Image Widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,81 +1,17 @@
 import 'package:flutter/material.dart';
 
-// class Home extends StatelessWidget {
-//   const Home({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Container(
-//         color: Colors.amberAccent,
-//         margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-//         padding: EdgeInsets.all(50),
-//         child: Text(
-//           "hello world",
-//           style: TextStyle(
-//             fontSize: 30,
-//             color: Colors.red,
-//             fontWeight: FontWeight.bold,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // return Row(
     return Column(
-      // mainAxisAlignment: MainAxisAlignment.start,
-      // mainAxisAlignment: MainAxisAlignment.center,
-      // mainAxisAlignment: MainAxisAlignment.end,
-      // mainAxisAlignment: MainAxisAlignment.spaceAround,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment:MainAxisAlignment.center,
+      crossAxisAlignment:CrossAxisAlignment.stretch,
       children: [
-        Container(
-          color: const Color.fromARGB(255, 94, 255, 58),
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: const Text(
-            "hello world",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
-        Container(
-          color: const Color.fromARGB(255, 99, 64, 255),
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: const Text(
-            "hello world",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
-        Container(
-          color: const Color.fromARGB(255, 255, 64, 195),
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: const Text(
-            "hello world",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
+        Image.asset('assets/images/picture.png', width: 200, height: 200),
+        SizedBox(height: 20),
+        Image.network('https://static.vecteezy.com/system/resources/thumbnails/033/494/777/small/cute-chibi-girl-wearing-a-cat-hoodie-ai-generative-png.png', width: 200, height: 200),
       ]
     );
   }
