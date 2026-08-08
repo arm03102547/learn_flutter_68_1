ce83411e5116c5ef4b01efad4531c8936962d557
arm03102547 <arm03102547@gmail.com>
2026-08-08 11:08:57 +0700

    Step 3 Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,20 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Hollo World'));
+    return Center(
+      child: Container(
+        color: Colors.amberAccent,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: EdgeInsets.all(50),
+        child: Text(
+          "hello world",
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
