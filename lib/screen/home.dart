4443dc324c924812aff5c5dc3005d88866ba84b4
arm03102547 <arm03102547@gmail.com>
2026-08-08 14:32:25 +0700

    Step 7 Button Widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -5,14 +5,61 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment:MainAxisAlignment.center,
-      crossAxisAlignment:CrossAxisAlignment.stretch,
-      children: [
-        Image.asset('assets/images/picture.png', width: 200, height: 200),
+    return Center(
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        TextButton(
+          style:TextButton.styleFrom(foregroundColor: Colors.pink),
+          onPressed: () {
+            print('Pressed Textbutton');
+          },
+          child: Text(
+            'Chick Me',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
         SizedBox(height: 20),
-        Image.network('https://static.vecteezy.com/system/resources/thumbnails/033/494/777/small/cute-chibi-girl-wearing-a-cat-hoodie-ai-generative-png.png', width: 200, height: 200),
-      ]
+        FilledButton(
+          style: FilledButton.styleFrom(
+            foregroundColor: const Color.fromARGB(255, 255, 163, 194),
+            backgroundColor: const Color.fromARGB(255, 58, 160, 255),
+          ),
+          onPressed: () {
+            print('Pressed FilledButton');
+          },
+          child: Text(
+            'Click Me',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(height: 20),
+        OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            foregroundColor: const Color.fromARGB(255, 255, 58, 58),
+            side: BorderSide(color: const Color.fromARGB(255, 121, 4, 255), width: 2,),
+          ),
+          onPressed: () {
+            print('Pressed OutlinedButton');
+          },
+          child: Text(
+            'Click Me',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(height: 20),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: const Color.fromARGB(255, 13, 1, 5),
+            backgroundColor: const Color.fromARGB(255, 247, 247, 248),
+          ),
+          onPressed: () {
+            print('Pressed ElevatedButton');
+          },
+          child: Text(
+            'Click Me',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],),
     );
   }
 }
