5fe1759978477048c05646dac3a884bb673fdf1e
arm03102547 <arm03102547@gmail.com>
2026-08-15 10:42:55 +0700

    Step 9 ListView Widget

--- lib/screen/item.dart
+++ lib/screen/item.dart
@@ -8,61 +8,26 @@ class Item extends StatefulWidget {
 }
 
 class _ItemState extends State<Item> {
-  int quantity = 10;
-
-  void incrementQuantity() {
-    setState(() {
-      quantity++;
-    });
-  }
-
-  void decrementQuantity() {
-    setState(() {
-      if (quantity > 0) {
-        quantity--;
-      }
-    });
-  }
+  List data = ["สมชาย", "สมหญิง", "สมปอง", "สมศรี", "สมจิต"];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // แสดงจำนวน
-          Text(
-            'Quantity: $quantity',
-            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return ListView.builder(
+      itemCount: data.length,
+      itemBuilder: (context, index) {
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: const Color.fromARGB(255, 243, 33, 138), width: 2),
+            borderRadius: BorderRadius.circular(10),
           ),
-
-          const SizedBox(height: 20),
-
-          // ปุ่ม Decrement และ Increment
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: decrementQuantity,
-                child: const Text(
-                  'Decrement',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-
-              const SizedBox(width: 20),
-
-              ElevatedButton(
-                onPressed: incrementQuantity,
-                child: const Text(
-                  'Increment',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
+          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          padding: EdgeInsets.all(30),
+          child: Text(
+            data[index],
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
